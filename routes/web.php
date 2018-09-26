2fe28de390076ff1ab8c30a456d68d6f69378e32
Michael Harley <michael.tandio@gmail.com>
2018-09-26 09:39:22 +0700

    260918 6-7

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Task;
 
-Route::get('/', function () {
-    $tasks = [
-        'Go to the starbuck',
-        'Go to school', 
-        'Go do homework'
-    ];
-    return view('welcome', compact('tasks'));
+Route::get('/tasks', function () {
+    $tasks = Task::all();
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+    $task = Task::find($id);
+    return view('tasks.show', compact('task'));
 });
